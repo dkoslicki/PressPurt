f6f867ae5de51e65fa2707de0bde4a23d2b8e6e5
dkoslicki <dmkoslicki@gmail.com>
2018-09-26 16:31:50 -0700

    Finish with equation 3.5, all tests passing

--- src/NumSwitch.py
+++ src/NumSwitch.py
@@ -1,9 +1,6 @@
 # This script will implement equations 3.4, 3.5, and 3.6
 import numpy as np
 
-# test matrix
-Atri = np.array([[-0.237, -1, 0, 0], [0.1, -0.015, -1, 0], [0, 0.1, -0.015, -1], [0, 0, 0.1, -0.015]])
-Atriinv = np.linalg.inv(Atri)
 
 def ind_switch(Ainv, eps, i, j, k, l):
 	"""
@@ -27,4 +24,33 @@ def ind_switch(Ainv, eps, i, j, k, l):
 	else:
 		raise Exception("Invalid sherman morrison ratio: %f" % sherm_ratio)
 
-print(ind_switch(Atriinv, .01, 0,1,2,3))
+
+def NS(Ainv, eps, k, l):
+	"""
+	This function implements equation 3.5: gives the number of sign switches
+	:param Ainv: inverse input matrix, numpy array
+	:param eps: perturbation size (scalar)
+	:param k: index
+	:param l: inex
+	:return: Natural number
+	"""
+	n = Ainv.shape[0]
+	if any([index >= n for index in [k, l]]):
+		raise Exception("Matrix is only %dx%d, invalid choice of subscripts: %d, %d." % (n, n, k, l))
+	ns_sum = 0
+	for i in range(n):
+		for j in range(n):
+			ns_sum += ind_switch(Ainv, eps, i, j, k, l)
+	return ns_sum
+
+
+# test cases
+# test matrix
+Atri = np.array([[-0.237, -1, 0, 0], [0.1, -0.015, -1, 0], [0, 0.1, -0.015, -1], [0, 0, 0.1, -0.015]])
+Aigp = np.array([[-0.237, -1, 0, 0], [0.1, -0.015, -1, -1], [0, 0.1, -0.015, -1], [0, .045, 0.1, -0.015]])
+Atriinv = np.linalg.inv(Atri)
+Aigpinv = np.linalg.inv(Aigp)
+assert NS(Aigpinv, -.2, 3, 2) == 9
+assert NS(Aigpinv, .2, 3, 2) == 1
+assert NS(Aigpinv, -.5, 1, 2) == 16
+assert NS(Aigpinv, .5, 1, 2) == 1
