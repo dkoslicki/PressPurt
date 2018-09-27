fe0fcfbda4a4aa3b68dd74b5eba056cddbd04fa8
dkoslicki <dmkoslicki@gmail.com>
2018-09-26 17:15:06 -0700

    Custom pdfs are hard... so let's leave this a mess for now

--- src/NumSwitch.py
+++ src/NumSwitch.py
@@ -1,6 +1,8 @@
 # This script will implement equations 3.4, 3.5, and 3.6
 import numpy as np
-
+import scipy.integrate as integrate
+import scipy.stats as st
+from scipy import special
 
 def ind_switch(Ainv, eps, i, j, k, l):
 	"""
@@ -43,6 +45,24 @@ def NS(Ainv, eps, k, l):
 			ns_sum += ind_switch(Ainv, eps, i, j, k, l)
 	return ns_sum
 
+def exp_num_switch(A, Ainv, k, l, dist=None):
+	"""
+	This implements equation 3.6: the expected number of sign switches
+	:param A: The original input matrix (numpy array)
+	:param Ainv: The inverse of A (pre-computed), numpy array
+	:param k: index
+	:param l: index
+	:param dist: A probability density function: function of a single argument
+	:return:
+	"""
+	if A[k, l] == 0:
+		raise Exception("You can only perturb non-zero entries: A[%d, %d] is zero." % (k, l))
+	# use a default normal distribution
+	# TODO: make sure this stays in the region of stability
+	if not dist:
+		# TODO: put custom pdf in here
+
+
 
 # test cases
 # test matrix
@@ -54,3 +74,38 @@ assert NS(Aigpinv, -.2, 3, 2) == 9
 assert NS(Aigpinv, .2, 3, 2) == 1
 assert NS(Aigpinv, -.5, 1, 2) == 16
 assert NS(Aigpinv, .5, 1, 2) == 1
+
+
+exp_num_switch(Atri, Atriinv, 0, 0)
+
+A = Atri
+Ainv = Atriinv
+k = 0
+l = 0
+
+class my_pdf(st.rv_continuous):
+	c = A[k, l]
+	if A[k, l] > 0:
+		max_pert = np.inf
+		min_pert = -A[k, l]
+	else:
+		max_pert = -A[k, l]
+		min_pert = -np.inf
+
+	def _pdf(self, x):
+		a = self.min_pert
+		b = self.max_pert
+		c = self.c
+		if a < x and x <= b:
+			# this is the result of Mathematica: PDF[TruncatedDistribution[{a, b}, NormalDistribution[c, 1]], x]
+			numerator = np.exp(-1 / 2. * (-c + x) ** 2)
+			denom = np.sqrt(2 * np.pi) * (
+						-1 / 2. * special.erfc((-a + c) / (np.sqrt(2))) + 1 / 2. * special.erfc((-b + c) / np.sqrt(2)))
+			value = numerator / float(denom)
+			return value
+		else:
+			return 0
+
+my_cv = my_pdf(a=my_pdf.min_pert, b=my_pdf.max_pert, name='my_pdf')
+
+print(integrate.quad(my_pdf, -np.inf, np.inf))
