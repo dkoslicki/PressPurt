1bb2c0db01ec05dcb0982a98fbc6720e3915b5fd
dkoslicki <dmkoslicki@gmail.com>
2018-09-27 16:08:44 -0700

    Finish with the region of stability thingy

--- src/NumSwitch.py
+++ src/NumSwitch.py
@@ -46,7 +46,7 @@ def NS(Ainv, eps, k, l):
 	return ns_sum
 
 
-def interval_of_stability(A, Ainv, k, l, max_bound=10):
+def interval_of_stability(A, Ainv, k, l, max_bound=10, num_sample=100):
 	"""
 	This function will return the interval of stability when perturbing just the (k,l) entry of A
 	:param A: numpy array
@@ -54,17 +54,108 @@ def interval_of_stability(A, Ainv, k, l, max_bound=10):
 	:param k: index
 	:param l: index
 	:param max_bound: some are always stable on one side, only go out this far), scalar > 0
+	:param num_sample: number of points (natural number) to sample to determine stability
 	:return: (a:scalar, b:scalar) the largest interval where A[k,l]+eps is stable
 	"""
-	if A[k, l] > 0 and Ainv[k, l] < 0:  # pos neg
+	# Input matrix needs to be stable
+	[s, _] = np.linalg.eig(A)
+	if not all([np.real(i) < 0 for i in s]):
+		raise Exception("The input matrix is not stable itself (one or more eigenvalues have non-negative real part). Cannot continue analysis.")
+
+	# Find an initial region of stability. Use contrapositive of theorem 3.1
+	if A[k, l] > 0 and Ainv[l, k] < 0:  # pos neg
 		initial_interval = (-A[k, l], -1/float(Ainv[l, k]))
-	if A[k, l] < 0 and Ainv[k, l] > 0:  # neg pos
+	if A[k, l] < 0 and Ainv[l, k] > 0:  # neg pos
 		initial_interval = (-1 / float(Ainv[l, k]), -A[k, l])
-	if A[k, l] < 0 and Ainv[k, l] < 0:  # neg neg
+	if A[k, l] < 0 and Ainv[l, k] < 0:  # neg neg
 		initial_interval = (-max_bound, min([-1 / float(Ainv[l, k]), -A[k, l]]))
-	if A[k, l] > 0 and Ainv[k, l] > 0:  # pos pos
+	if A[k, l] > 0 and Ainv[l, k] > 0:  # pos pos
 		initial_interval = (max([-1 / float(Ainv[l, k]), -A[k, l]]), max_bound)
-	return initial_interval
+	# now need to sample to determine the actual region of stability
+	# we'll basically do a grid search and look for the real parts of the eigenvalues begin negative
+	to_sample = np.linspace(initial_interval[0], initial_interval[1], num_sample)
+	#return to_sample
+	zero_matrix = np.zeros(A.shape)  # matrix we will be perturbing by
+	eig_values = []
+	for pert_val in to_sample:
+		zero_matrix[k, l] = pert_val  # update the perturb value
+		[s, _] = np.linalg.eig(A + zero_matrix)
+		eig_values.append(max([np.real(i) for i in s]))  # look at the largest eigenvalue
+	#return eig_values
+	# now, return the largest interval where all the eigenvalues have negative real part
+	zero_loc = int(np.argmin(np.abs(to_sample)))
+	upper_bound = initial_interval[1]
+	for i in range(len(to_sample) - zero_loc):
+		if eig_values[zero_loc + i] >= 0:
+			upper_bound = to_sample[zero_loc + i - 1]
+			break
+	lower_bound = initial_interval[0]
+	for i in range(zero_loc):
+		if eig_values[zero_loc - i] >= 0:
+			lower_bound = to_sample[zero_loc - i + 1]
+			break
+	return (lower_bound, upper_bound)
+
+
+
+def interval_of_stability_crawl(A, Ainv, k, l, max_bound=10, step_size=.0001):
+	"""
+	This function will return the interval of stability when perturbing just the (k,l) entry of A
+	Difference with this implementation is that it decides to crawl out from the origin, looking for instability
+	:param A: numpy array
+	:param Ainv: inverse of A
+	:param k: index
+	:param l: index
+	:param max_bound: some are always stable on one side, only go out this far), scalar > 0
+	:param step_size: step size for perturbation values
+	:return: (a:scalar, b:scalar) the largest interval where A[k,l]+eps is stable
+	"""
+	# Input matrix needs to be stable
+	[s, _] = np.linalg.eig(A)
+	if not all([np.real(i) < 0 for i in s]):
+		raise Exception("The input matrix is not stable itself (one or more eigenvalues have non-negative real part). Cannot continue analysis.")
+
+	# Find an initial region of stability. Use contrapositive of theorem 3.1
+	if A[k, l] > 0 and Ainv[l, k] < 0:  # pos neg
+		initial_interval = (-A[k, l], -1/float(Ainv[l, k]))
+	if A[k, l] < 0 and Ainv[l, k] > 0:  # neg pos
+		initial_interval = (-1 / float(Ainv[l, k]), -A[k, l])
+	if A[k, l] < 0 and Ainv[l, k] < 0:  # neg neg
+		initial_interval = (-max_bound, min([-1 / float(Ainv[l, k]), -A[k, l]]))
+	if A[k, l] > 0 and Ainv[l, k] > 0:  # pos pos
+		initial_interval = (max([-1 / float(Ainv[l, k]), -A[k, l]]), max_bound)
+	# now need to sample to determine the actual region of stability
+	# we'll basically do a grid search and look for the real parts of the eigenvalues begin negative
+	# crawl to the right
+	zero_matrix = np.zeros(A.shape)
+	eps = step_size
+	all_neg_eigs = True
+	while all_neg_eigs is True:
+		zero_matrix[k, l] = eps
+		[s, _] = np.linalg.eig(A + zero_matrix)
+		max_eig_val = max([np.real(i) for i in s])
+		if max_eig_val >= 0 or eps >= initial_interval[1]:
+			eps -= step_size
+			all_neg_eigs = False
+		else:
+			eps += step_size
+	upper_bound = eps
+	# crawl to the right
+	zero_matrix = np.zeros(A.shape)
+	eps = -step_size
+	all_neg_eigs = True
+	while all_neg_eigs is True:
+		zero_matrix[k, l] = eps
+		[s, _] = np.linalg.eig(A + zero_matrix)
+		max_eig_val = max([np.real(i) for i in s])
+		if max_eig_val >= 0 or eps <= initial_interval[0]:
+			eps += step_size
+			all_neg_eigs = False
+		else:
+			eps -= step_size
+	lower_bound = eps
+	return (lower_bound, upper_bound)
+
 
 
 
