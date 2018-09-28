3e8edba0da9a0aeedb30d8ed9b701393bd55b88f
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-09-28 15:54:15 -0700

    Continue working on SS, got the symbolic inverse divided by the original inverse

--- src/SS.py
+++ src/SS.py
@@ -4,19 +4,8 @@ import numpy as np
 import sympy as sp
 import scipy.stats as st
 from NumSwitch import interval_of_stability
-#entries_to_perturb = np.ones((4,4))
-entries_to_perturb = np.array([[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
-Aigp = np.array([[-0.237, -1, 0, 0], [0.1, -0.015, -1, -1], [0, 0.1, -0.015, -1], [0, .045, 0.1, -0.015]])
-Aigpinv = np.linalg.inv(Aigp)
-pert_locations_i, pert_locations_j = np.where(entries_to_perturb)
-symbol_string = ""
-for i,j in zip(pert_locations_i, pert_locations_j):
-	symbol_string += "eps_%d_%d" % (i, j)
-symbol_tup = sp.symbols(symbol_string)
 
-# for an initial approach, we will take a uniform distribution with a specified length, that don't change the sign
 
-A = Aigp
 def intervals(aij, x=0.01):
 	"""
 	This function defines an interval of size int_size around aij without changing the sign of aij
@@ -50,6 +39,47 @@ def intervals(aij, x=0.01):
 			upper_bound = np.abs(aij)
 		#upper_bound = np.piecewise(aij, [x <= 2*np.abs(aij), x >= 2*np.abs(aij)], [x/2., np.abs(aij)])
 	return [lower_bound, upper_bound]
+
+
+# TODO: will also want a function that gives me 1 or 0 depending on stability/non-stability
+
+#entries_to_perturb = np.ones((4,4))
+entries_to_perturb = np.array([[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+A = sp.Matrix(np.array([[-0.237, -1, 0, 0], [0.1, -0.015, -1, -1], [0, 0.1, -0.015, -1], [0, .045, 0.1, -0.015]]))
+Ainv = sp.Matrix(A.inv())
+
+# get the variables we are going to perturb
+pert_locations_i, pert_locations_j = np.where(entries_to_perturb)
+symbol_string = ""
+for i, j in zip(pert_locations_i, pert_locations_j):
+	symbol_string += "eps_%d_%d" % (i, j)
+if len(pert_locations_i) == 1:
+	symbol_tup = [sp.symbols(symbol_string)]
+else:
+	symbol_tup = list(sp.symbols(symbol_string))
+
+# create the matrix with the symbolic perturbation values
+B_temp = sp.Matrix(np.zeros(A.shape))
+iter = 0
+for i, j in zip(pert_locations_i, pert_locations_j):
+	B_temp[i, j] = symbol_tup[iter]
+	iter += 1
+
+# form the symbolic matrix (A+B)^(-1)./A^(-1)
+B = sp.Matrix(B_temp)
+AplusB = A + B
+AplusBinv = AplusB.inv()
+AplusBinvDivAinv = sp.Matrix(np.zeros(A.shape))
+for i in range(A.shape[0]):
+	for j in range(A.shape[1]):
+		AplusBinvDivAinv[i, j] = AplusBinv[i, j] / Ainv[i, j]
+
+# lambdafy the symbolic quantity
+AplusBinvDivAinveval = sp.lambdify(symbol_tup, AplusBinvDivAinv, "numpy")
+
+# if any of the entries of AplusBinvDivAinveval are < 0, then a sign switch has occurred
+
+
 
 
 
