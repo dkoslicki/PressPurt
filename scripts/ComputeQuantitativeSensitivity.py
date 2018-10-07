71697e103d2806f834395bfc5452d9ba65ee1287
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-10-06 17:15:25 -0700

    Added script to compute the quantitative sensitivity

---
+++ scripts/ComputeQuantitativeSensitivity.py
@@ -0,0 +1,68 @@
+import argparse
+import numpy as np
+import os
+import sys
+import pickle
+import timeit
+from multiprocessing import Pool  # Much faster without dummy (threading)
+import multiprocessing
+import itertools
+
+# import stuff in the src folder
+try:
+	import MRS
+except ImportError:
+	sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
+	import MRS
+try:
+	import SS
+except ImportError:
+	sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
+	import SS
+try:
+	import NumSwitch
+except ImportError:
+	sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
+	import NumSwitch
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="This script Generates the quantitative sensitivity: perturbing each single entry off to infinity.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('input_file', type=str, help="Input comma separated file for the jacobian matrix.")
+	parser.add_argument('output_folder', type=str, help="Output folder. A number of files will be created in the form 'output_folder/<prefix>_*.npy'")
+	parser.add_argument('-p', '--prefix', help="Prefix of output files, if you so choose.", default=None)
+
+	# read in the arguments
+	args = parser.parse_args()
+	input_file = os.path.abspath(args.input_file)
+	output_folder = args.output_folder
+	if output_folder:
+		output_folder = os.path.abspath(output_folder)
+	prefix = args.prefix
+
+	if not os.access(output_folder, os.W_OK):
+		raise Exception("The provided directory %s is not writable." % output_folder)
+
+	if prefix:
+		quant_sens_file = os.path.join(output_folder, prefix + "_quantitative_sensitivity.csv")
+	else:
+		quant_sens_file = os.path.join(output_folder, "quantitative_sensitivity.csv")
+
+	# read in the input matrix
+	A = np.loadtxt(input_file, delimiter=",")
+	Ainv = np.linalg.inv(A)
+	m, n = A.shape
+
+	# make sure the original matrix is itself asymptotically stable
+	if not SS.is_stable(A):
+		raise Exception("Sorry, the input matrix is not stable itself (all eigenvalues must have negative real part). Please try again.")
+
+	# do the computation
+	quant_sens_values = np.zeros((m, n))
+	for i in range(m):
+		for j in range(n):
+			if A[i, j] != 0:  # only perturb non-zero entries
+				quant_sens_values[i, j] = MRS.quant_sens(A, i, j)
+			else:
+				quant_sens_values[i, j] = 0
+
+	np.savetxt(quant_sens_file, quant_sens_values, delimiter=',')
